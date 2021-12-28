51f94c6a4786c6d16f2d4045b8808bedae61e066
Pier Stoyanov <pier.stoyanov@gmail.com>
2021-12-28 14:54:14 +0200

    fixed model

--- ReactJS-CP/Models/Invoice.cs
+++ ReactJS-CP/Models/Invoice.cs
@@ -9,13 +9,13 @@ namespace ReactJS_CP.Models
         public string InvoiceId { get; set; }
         public int Number { get; set; }
         public DateTime WhenCreated { get; set; }
-        public Company IssuerId { get; set; }
-        public Company ReceiverId { get; set; }
+        public int IssuerId { get; set; }
+        public int ReceiverId { get; set; }
         //TODO ADD Invoice items
         public string MaterialsId { get; set; }
         public string MaterialsQuantity { get; set; }
         public string MaterialsPrices { get; set; }
         public int AccountantId { get; set; }
-        public Accountant Accountant { get; set; }
+
     }
 }
